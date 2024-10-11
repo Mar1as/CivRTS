b7a64d4492e3513d4e7efc37d2fbbf08b712a486
Mar1as <martin-noseki@email.cz>
2024-10-11 13:56:22 +0200

    Nefunguje shader

--- Assets/Hex/New/DataHexCell.cs
+++ Assets/Hex/New/DataHexCell.cs
@@ -25,6 +25,8 @@ public class DataHexCell
     public HexCoordinates coordinates = new HexCoordinates(0,0);
     [SerializeField]
     public MainHexCell[] neighbours = new MainHexCell[6];
+    [SerializeField]
+    public Color color;
 
     public DataHexCell()
     {
